814f47594390204c30d2f31771962084c7040e7f
brian <yuni0914@163.com>
2017-08-14 11:22:26 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -135,6 +137,7 @@ public class WeatherActivity extends AppCompatActivity {
                     }
                 });
             }
+
             //请求成功
             @Override
             public void onResponse(Call call, Response response) throws IOException {
@@ -202,6 +205,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
@@ -213,8 +218,9 @@ public class WeatherActivity extends AppCompatActivity {
             //请求失败
             @Override
             public void onFailure(Call call, IOException e) {
-
+                e.printStackTrace();
             }
+
             //请求成功
             @Override
             public void onResponse(Call call, Response response) throws IOException {
